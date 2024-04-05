bec3228286f612755506f54d882168d9ee7a5292
kalipso <131648818+kalipsomasya@users.noreply.github.com>
2024-04-05 11:10:02 +0300

    gandrīz + style

--- lpp2.js
+++ lpp2.js
@@ -68,4 +68,9 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
   //laiks
-  
+  function updateDateTime() {
+    const now = new Date();
+    const currentDateTime = now.toLocaleString();
+    document.querySelector('#datetime').textContent = currentDateTime;
+  };
+  setInterval(updateDateTime, 1000);
